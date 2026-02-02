39e6571f6a22fdba5f5c2d7a9dd29fbee27335d6
dev-k99 <kwanelerh069@gmail.com>
2026-02-02 23:42:04 +0200

    Email Service

---
+++ LibraryManagement.Core/Interfaces/IEmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Core.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendBookAvailableEmailAsync(string userEmail, string userName, string bookTitle);
+        Task SendReservationReadyEmailAsync(string userEmail, string userName, string bookTitle);
+        Task SendLoanDueSoonEmailAsync(string userEmail, string userName, string bookTitle, DateTime dueDate);
+        Task SendOverdueNoticeEmailAsync(string userEmail, string userName, string bookTitle, DateTime dueDate);
+    }
+}
