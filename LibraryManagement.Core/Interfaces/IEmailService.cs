1e0af4e4b2dbff6dd20aec398d480668a203b842
dev-k99 <kwanelerh069@gmail.com>
2026-02-04 22:11:50 +0200

    Email services

--- LibraryManagement.Core/Interfaces/IEmailService.cs
+++ LibraryManagement.Core/Interfaces/IEmailService.cs
@@ -12,5 +12,7 @@ namespace LibraryManagement.Core.Interfaces
         Task SendReservationReadyEmailAsync(string userEmail, string userName, string bookTitle);
         Task SendLoanDueSoonEmailAsync(string userEmail, string userName, string bookTitle, DateTime dueDate);
         Task SendOverdueNoticeEmailAsync(string userEmail, string userName, string bookTitle, DateTime dueDate);
+        Task SendPasswordResetEmailAsync(string email, string userName, string resetCode);
+        Task SendPasswordResetConfirmationAsync(string email, string userName);
     }
 }
