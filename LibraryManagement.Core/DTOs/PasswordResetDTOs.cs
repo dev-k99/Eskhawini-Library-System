d0700347e6f9cec30d4391ef507e8aa33be7b4fe
dev-k99 <kwanelerh069@gmail.com>
2026-02-04 22:12:58 +0200

    Changed DTOs

---
+++ LibraryManagement.Core/DTOs/PasswordResetDTOs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Core.DTOs
+{
+    public class RequestPasswordResetRequest
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+
+    public class ResetPasswordRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string ResetCode { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+
+    public class PasswordResetResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
